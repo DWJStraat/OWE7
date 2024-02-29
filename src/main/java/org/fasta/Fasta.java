a429552f6061c78799ad47a65080d0ef00e89ea7
DWJStraat <dstraat1@gmail.com>
2024-02-29 14:54:08 +0100

    Wrote first version of Fasta Reader

---
+++ src/main/java/org/fasta/Fasta.java
@@ -0,0 +1,10 @@
+package org.fasta;
+
+public class Fasta {
+    private final String head;
+    private final String seq;
+    public Fasta (String header, String sequence) {
+        head = header;
+        seq = sequence;
+    }
+}
---
+++ src/main/java/org/fasta/FastaReader.java
@@ -0,0 +1,35 @@
+package org.fasta;
+
+import org.logger.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FastaReader {
+    private final Log log = new Log(FastaReader.class.getName());
+    private File file;
+    final List<Fasta> fastas = new ArrayList<>();
+
+    public FastaReader(String path) throws IOException {
+        log.info("Reading file: " + path);
+        file = new File(path);
+        String contents = new String(java.nio.file.Files.readAllBytes(file.toPath()));
+        StringBuilder sequence = new StringBuilder();
+        String header = "";
+        for (String line : contents.split("\n")) {
+            log.info("Reading line: " + line);
+            if (line.startsWith(">")) {
+                if (!sequence.isEmpty()) {
+                    fastas.add(new Fasta(header, sequence.toString()));
+                    sequence = new StringBuilder();
+                }
+                header = line;
+            } else {
+                sequence.append(line);
+            }
+        }
+
+    }
+}
